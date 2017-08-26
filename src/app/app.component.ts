50d932f80b32098886b076d5d3d1ee252c7c9195
unknown <bandara00007@gmail.com>
2017-08-26 12:44:55 +0530

    student detail page added

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,7 +1,52 @@
 import { Component } from '@angular/core';
 
+export class Student{
+	id: number;
+	name: string;
+}
+
+const STUDENTS: Student[] = [
+  { id: 1, name: 'Mahesh' },
+  { id: 2, name: 'Anuradha' },
+  { id: 3, name: 'Bandara' },
+  { id: 4, name: 'Kumuduni' },
+  { id: 5, name: 'Damayanthi' }
+];
+
+
 @Component({
   selector: 'my-app',
-  template: `<h1>Hello {{name}}</h1>`,
+  template: `
+  <h1>{{title}}</h1>
+    <h2>Students list</h2>
+    <ul class="students">
+      <li *ngFor="let student of students"
+        [class.selected]="student === selectedStudent"
+        (click)="onSelect(student)">
+        <span class="badge">{{student.id}}</span> {{student.name}}
+      </li>
+    </ul>
+
+    <div *ngIf="selectedStudent">
+      <h2>{{selectedStudent.name}} details!</h2>
+      <div><label>id: </label>{{selectedStudent.id}}</div>
+      <div>
+        <label>name: </label>
+        <input [(ngModel)]="selectedStudent.name" placeholder="name"/>
+      </div>
+    </div>
+    `
 })
-export class AppComponent  { name = 'Angular'; }
+export class AppComponent  {
+	title = "Student management system"
+	students = STUDENTS;
+	selectedStudent : Student;
+	student : Student ={
+		id:1,
+		name : "Anuradha"
+	} 
+
+	onSelect(student:Student):void{
+		this.selectedStudent = student;
+	}
+}
